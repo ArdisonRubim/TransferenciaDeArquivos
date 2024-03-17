33f717cbc1cdf1ef58118da133c2a4ca24109564
Ardison Rubim <ardison.rubim@gmail.com>
2024-03-17 17:02:34 -0300

    Versão 2 atualizada

--- cria_script_transfer_2.0.sh
+++ cria_script_transfer_2.0.sh
@@ -1,50 +1,84 @@
 #!/bin/bash
 
-##########################################################
-# Author: Ardison Rubim        				 #
-#                              				 #
-# Date: 27/02/2024                                       #
-# Update: 17/03/2024                                     #
-# Description: Script para gerar#
-# script de transferência de ar-#
-# quivos.                       #
-#################################
+############################################################
+# Author: Ardison Rubim        				   #
+# Linkedin: https://www.linkedin.com/in/ardison-rubim/     #
+# WhatsApp: (98) 98497-6673				   #
+#                              				   #
+# Date: 27/02/2024     					   #
+#							   #
+# Author: Ardison Rubim					   #
+# Update02: 17/03/2024 - Ardison Rubim			   #
+#							   #
+# Description01: Script para gerar script de transferência #
+# de arquivos.  					   #
+#							   #
+# Description02: Foi incluido função de percorrer a pasta  #
+# de origem, retirando do técnico o trabalho de validar    #
+# a quantidade de pastas.			    	   #
+# 				                           #
+############################################################
 
 
-echo "Verificando o espaço..."
+echo "#!/bin/bash" > transfer.sh
+echo "" >> transfer.sh
 
-# Especifique o caminho de destino dos arquivo
-FILE="/mnt/examesantigos3/2023/12/"
+###############################################################
+#-------------------------------------------------------------#
+# Gerando Logo					              #
+echo " echo ' ____   ___  _     ___  ____ '  " >> transfer.sh
+echo " echo '|  _  |  _ /| |   / _ \/ ___|'  " >> transfer.sh
+echo " echo '| |_) | | | | |  | | |/\___/\'  " >> transfer.sh
+echo " echo '|  __/| |_| | |__| |_| |___) |' " >> transfer.sh
+echo " echo '|_|    \___/|_____\___/|____/'  " >> transfer.sh
+echo ''>> transfer.sh
+echo "sleep 2" >> transfer.sh
 
+echo "" >> transfer.sh
+#-------------------------------------------------------------#
+###############################################################
+
+
+# Verifique o nome da atual do script
+SCRIPT="cria_script_transfer_2.0.sh"
+
+echo "Verificando o espaço no diretório atual..."
 # Total de arquivos
 TOTAL=$(du -sh)
 
-# Caminho de origem
-FILEPWD=$(pwd)
+# Especifique o caminho de destino dos arquivo
+FILE="/mnt/examesantigos3/2023/1/"
+
+# Especifique o caminho de origem
+#FILEPWD="/mnt/2023/1"
+FILEPWD=$(ls /mnt/2023/1 | sort -n)
 
 echo "Total a ser transferido: '$TOTAL'"
 sleep 2
 echo ""
+
+
 # Comando para gerar script
-# O laco for vai executar a quantidades de vezes que colocar entre {1..31}
-# No exemplo ele vai rodar de 1 a 31 guardando o valor na variavel pasta
-# Ou seja vai gerar 31 linhas com as patas de 1 a 31
-
-
-echo "#!/bin/bash" > transfer.sh
 echo "#Total: "$TOTAL" " >> transfer.sh
 echo "" >> transfer.sh
 echo "Gerando script de transferencia"
 echo ""
 echo "" >> transfer.sh
-#Colque o intervalo de pasta no local mostrado abaixo
+
+#Variável FILEPWD abaixo já guarda o caminho de origem
 #------------------------------------------------------#
-#Qtd de pastas↓ a ↓serem criadas 31#
-for pasta in "$FILEPWD"; do
-	echo "Criando script, pasta $pasta "
-	sleep 0.3
+#------------------------------------------------------#
+
+# Loop para percorrer as pastas na pasta de origem
+for pasta in $FILEPWD; do
+    # Verifica se o nome da pasta é diferente dos scripts gerados
+    if [[ $pasta != "transfer.sh" && $pasta != "$SCRIPT" ]]; then
+        echo "Criando script, pasta $pasta "
+        sleep 0.3
         echo "rsync -havPuz $pasta/* $FILE$pasta/" >> transfer.sh
+    fi
 done
+
 echo "" >> transfer.sh
 echo "echo 'Transferencia Concluida!'" >> transfer.sh
 
@@ -57,3 +91,4 @@ echo ""
 echo 'Script transfer.sh criado com sucesso!'
 echo 'Por favor, valide antes de executar'
 sleep 3
+
